07705eef087a095941ac2606b862ca63a5bc0dde
Alexander <139145315+Zaandros@users.noreply.github.com>
2024-01-08 14:45:53 +0000

    Added in functionality to rubber fixed bug where changing colour didn't work

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,17 @@
 
+//Variables
 
 let height = 10;
 let width = 10;
 let gridSize = 16;
+let cell = document.createElement('div');
+let colourConfirmed = false;
+let blackSelected = true;
+let randomSelected = false;
+let erase = false;
+
+//Query Selectors
+
 const grid = document.querySelector('.grid');
 const smallBtn = document.querySelector(".small");
 const mediumBtn = document.querySelector(".medium");
@@ -10,16 +19,32 @@ const largeBtn = document.querySelector(".large");
 const sizeSlider = document.querySelector(".slider");
 const sizeDisplay = document.querySelector(".canvas-size");
 const clearButton = document.querySelector(".clear");
+const colourBlack = document.querySelector(".black");
+const colourPicker = document.querySelector(".colour-picker");
+const confirmColour = document.querySelector(".confirm");
+const confirmRandom = document.querySelector(".random");
+const rubber = document.querySelector(".rubber");
+let colourValue = document.querySelector(".hovering");
 
+
+//event listeners
 
 smallBtn.addEventListener("click", smallSize);
 mediumBtn.addEventListener("click", mediumSize);
 largeBtn.addEventListener("click", largeSize);
 sizeSlider.addEventListener("change", sizeSelector);
 clearButton.addEventListener("click", clearCanvas);
+colourBlack.addEventListener("click", selectColourBlack);
+confirmColour.addEventListener("click", selectColourPicker);
+confirmRandom.addEventListener("click", selectRandomColour);
+rubber.addEventListener("click", rubout);
 
+//section functions are called
 
 drawGrid();
+
+
+//functions
 
 function smallSize() {
   gridSize = 16;
@@ -57,7 +82,7 @@ function drawGrid() {
 
     for (let i = 0; i < boardSize; i++) {
 
-        let cell = document.createElement('div');
+        cell = document.createElement('div');
         cell.classList.add(`cell`);       
         cell.addEventListener("mouseenter", detectMouse);
         grid.insertAdjacentElement("beforeend", cell);
@@ -67,7 +92,41 @@ function drawGrid() {
 }
 
 function detectMouse(value) {
-  value.target.classList.add("hovering");
+  if (blackSelected == true) {
+  //cell.style.backgroundColor = "black";
+  value.target.classList.add("hoveringBlack");
+  value.target.classList.remove("hoveringYellow");
+  value.target.classList.remove("hoveringRandom");
+  value.target.classList.remove("delete");
+  
+  }
+
+  else if (colourConfirmed == true) {
+
+    value.target.classList.add("hoveringYellow");
+    value.target.classList.remove("hoveringBlack");
+    value.target.classList.remove("hoveringRandom");
+    value.target.classList.remove("delete");
+  
+  }
+  else if (randomSelected == true) {
+    
+    value.target.classList.add("hoveringRandom");
+    value.target.classList.remove("hoveringBlack");
+    value.target.classList.remove("hoveringYellow");
+    value.target.classList.remove("delete");
+
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+      for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+      }
+
+  }
+  else if (erase == true) {
+    value.target.classList.add("delete");
+  }
+ 
 }
 
 function clearCanvas() {
@@ -81,5 +140,35 @@ function reset() {
   }
 }
 
+ function selectColourPicker() {
+
+  colourConfirmed = true;
+  blackSelected = false;
+  randomSelected = false;
+  erase = false;
+}
+
+function selectColourBlack() {
+
+  blackSelected = true;
+  colourConfirmed = false;
+  randomSelected = false;
+  erase = false;
+}
+
+function selectRandomColour(){
+  randomSelected = true;
+  colourConfirmed = false;
+  blackSelected = false;
+  erase = false;
+}
+
+function rubout() {
+  erase = true;
+  blackSelected = false;
+  colourConfirmed = false;
+  randomSelected = false;
+  
+}
 
 
